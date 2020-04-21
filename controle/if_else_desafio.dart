abc7690f95032f272aa1145f1f713f3c3b007cab
montonurb <brunomoncar007@gmail.com>
2020-04-21 16:23:37 -0300

    aula 02 (desafio) - estrutura de controle

---
+++ controle/if_else_desafio.dart
@@ -0,0 +1,15 @@
+
+/**
+ * DESAFIO:
+ * PERGUNTA 1: O seguinte código será executado?
+ * PERGUNTA 2: Se sim, você percebeu algo errado? Comente!
+ * PERGUNTA 3: Se não, o que irá exibir?
+ */
+
+main() {
+  var nota = 0.3;
+  
+  if(nota >= 9.0);{
+    print('Parabéns! Você foi brilhante!');
+  }
+}
