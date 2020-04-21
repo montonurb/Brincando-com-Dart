f151748ef030f978aa9cf8e84dcd1fca415a288b
montonurb <brunomoncar007@gmail.com>
2020-04-21 15:47:10 -0300

    aula 18

---
+++ generics.dart
@@ -0,0 +1,13 @@
+main() {
+  List<String> frutas = ['banana', 'maça', 'laranja'];
+  frutas.add('melão');
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente': 10759.32,
+    'vendedor': 8692.41,
+    'estagiario': 1024.32,
+  };
+
+  print(salarios);
+}
