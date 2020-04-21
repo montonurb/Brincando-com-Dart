13458fea1e9bdaa16c0582d8e326441fd3759829
montonurb <brunomoncar007@gmail.com>
2020-04-21 13:02:07 -0300

    aula 16

---
+++ constantes_2.dart
@@ -0,0 +1,5 @@
+main() {
+  var lista = const ['Ana', 'Lia', 'Gui']; // desta forma com const, você fixa que não pode adicionar itens na lista.
+  //lista.add('Rebeca'); //se descomentar isso, vai dar erro por conta do const
+  print(lista);
+}
