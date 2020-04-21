e864a20ad38fdafc74d0c54ea6e6f50a76052141
montonurb <brunomoncar007@gmail.com>
2020-04-21 16:13:11 -0300

    organizando pastas

---
+++ controle/if_else.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+main() {
+  var nota = Random().nextInt(11);
+
+  if(nota >= 9){
+    print('Quador de honra! Sua nota é $nota.');
+  } else if(nota > 7){
+    print('Aprovado! Sua nota é $nota.');
+  } else if(nota >= 5){
+    print('Recuperação! Sua nota é $nota.');
+  } else if(nota >= 4){
+    print('Recuperação + Trabalho! Sua nota é $nota.');
+  } else {
+    print('Reprovado! Sua nota é $nota.');
+  }
+
+  print('\nOutra forma:\n');
+  if(nota >= 9){
+    print('Quador de honra! Sua nota é $nota.');
+  } else {
+    if (nota >= 7){
+      print('Aprovado! Sua nota é $nota.');
+    } else {
+      if(nota >= 5){
+        print('Recuperação! Sua nota é $nota.');
+      } else {
+        if (nota >= 4){
+          print('Recuperação + Trabalho! Sua nota é $nota.');
+        } else {
+          print('Reprovado! Sua nota é $nota.');
+        }
+      }
+    }
+  }
+}
