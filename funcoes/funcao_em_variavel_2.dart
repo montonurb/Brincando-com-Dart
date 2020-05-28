de6bf02782c203aca45f4e4fe189b744bbff24aa
Bruno Monteiro <brunomoncar007@gmail.com>
2020-05-28 16:45:36 -0300

    segundo exercicio de função

---
+++ funcoes/funcao_em_variavel_2.dart
@@ -0,0 +1,12 @@
+main() {
+  
+  var adicao = (int a, int b) => a + b;
+  var subtracao = (int a, int b) => a - b;
+  var multiplicacao = (int a, int b) => a * b;
+  var divisao = (int a, int b) => a / b;
+
+  print(adicao(10, 11));
+  print(subtracao(10, 5));
+  print(multiplicacao(10, 2));
+  print(divisao(10, 10));
+}
