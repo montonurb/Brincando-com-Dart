e9ed3292eb11bfea0a2b1034cc78054caa869204
Bruno Monteiro <brunomoncar007@gmail.com>
2020-06-01 11:08:00 -0300

    retornar função

---
+++ funcoes/retornar_funcao.dart
@@ -0,0 +1,17 @@
+ int Function(int) somaParcial(int a) {
+   int c = 0;
+
+   return (int b) {
+     return a + b + c;
+   };
+ }
+
+ main() {
+   print(somaParcial(2)(8));
+
+   var somaCom10 = somaParcial(10);
+
+   print(somaCom10(3));
+   print(somaCom10(7));
+   print(somaCom10(10));
+ }
